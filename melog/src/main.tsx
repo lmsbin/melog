8274d8aca20d74cc88e95b2b2380d522ae5a02a9
Jo hanbin <whgksqls9999@naver.com>
2024-12-31 21:58:27 +0900

    Update react version to 19.0.0

--- melog/src/main.tsx
+++ melog/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { App } from './app';
 import { BrowserRouter } from 'react-router-dom';
+import { App } from './06.application';
 
 createRoot(document.getElementById('root')!).render(
 	<BrowserRouter>
