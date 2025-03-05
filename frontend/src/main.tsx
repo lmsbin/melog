138f394506fb2f08baea3e385f3c5f28a61dcd65
Jo hanbin <whgksqls9999@naver.com>
2025-03-05 22:46:04 +0900

    [FE] move application.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import { Application } from './application';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<App />
+		<Application />
 	</StrictMode>
 );
