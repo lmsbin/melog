0aa3f5d36e7cc7d4de86f7ef9849b857cecf19fb
Jo hanbin <whgksqls9999@naver.com>
2025-03-11 21:15:13 +0900

    [FE] add entry point for test

---
+++ frontend/src/__test__/application/test_application.tsx
@@ -0,0 +1,5 @@
+import { memo } from 'react';
+
+export const TestApplication = memo(function TestApplication() {
+	return <div>테스트베드</div>;
+});
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,25 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Application } from './application';
+import { TestApplication } from './__test__';
 
-createRoot(document.getElementById('root')!).render(
-	<StrictMode>
-		<Application />
-	</StrictMode>
-);
+(function main() {
+	const dev_mode = import.meta.env.MODE;
+
+	const entry = getEntryPoint(dev_mode);
+
+	createRoot(document.getElementById('root')!).render(
+		<StrictMode>{entry}</StrictMode>
+	);
+})();
+
+function getEntryPoint(mode: string) {
+	switch (mode) {
+		case 'test':
+			return <TestApplication />;
+		case 'development':
+			return <Application />;
+	}
+
+	throw new Error(`There is no entry point for mode: '${mode}'`);
+}
