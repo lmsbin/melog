15d71870605e47cdd454b0a57985e68a0ca009c0
lms0806 <godmlzkf1@naver.com>
2025-03-23 19:55:07 +0900

    add : 사용자 세트 효과 적용되는 것들만 보여지도록 기능 추가

--- backend/src/api/character.rs
+++ backend/src/api/character.rs
@@ -337,12 +337,73 @@ pub async fn get_user_symbol_equipment(
 
     // 응답 결과 확인
     if response.status().is_success() {
-        let user_ability: Symbol = response
+        let user_symbol: Symbol = response
             .json()
             .await
             .expect("Failed to parse response JSON");
 
-        Ok(Json(user_ability))
+        Ok(Json(user_symbol))
+    } else {
+        Err((StatusCode::BAD_REQUEST, "Failed to fetch OCID"))
+    }
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct SetEffectInfoFull {
+    pub set_count: i8,
+    pub set_option: String,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct SetEffectInfo {
+    pub set_name: String,
+    pub total_set_count: i8,
+    pub set_option_full: Vec<SetEffectInfoFull>,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct SetEffect {
+    pub set_effect: Vec<SetEffectInfo>,
+}
+
+pub async fn get_user_set_effect(
+    Extension(api_key): Extension<Arc<API>>,
+) -> Result<Json<SetEffect>, (StatusCode, &'static str)> {
+    // POST 요청 보내기
+    let response = request_parser(api_key.clone(), "set-effect").await;
+
+    // 응답 결과 확인
+    if response.status().is_success() {
+        let user_effect: SetEffect = response
+            .json()
+            .await
+            .expect("Failed to parse response JSON");
+
+        let filtered_data = SetEffect {
+            set_effect: user_effect
+                .set_effect
+                .into_iter()
+                .filter_map(|set_info| {
+                    let matched_options: Vec<SetEffectInfoFull> = set_info
+                        .set_option_full
+                        .into_iter()
+                        .filter(|option| option.set_count == set_info.total_set_count)
+                        .collect();
+
+                    if matched_options.is_empty() {
+                        None
+                    } else {
+                        Some(SetEffectInfo {
+                            set_name: set_info.set_name,
+                            total_set_count: set_info.total_set_count,
+                            set_option_full: matched_options,
+                        })
+                    }
+                })
+                .collect(),
+        };
+
+        Ok(Json(filtered_data))
     } else {
         Err((StatusCode::BAD_REQUEST, "Failed to fetch OCID"))
     }
