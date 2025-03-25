2eb969763c1ce1cd0258fee827eeacd7bdd80be0
Jo hanbin <whgksqls9999@naver.com>
2025-03-25 20:29:37 +0900

    add: SearchPage

--- frontend/src/application/router.tsx
+++ frontend/src/application/router.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import { MainLayout } from '../common/page';
 import { MainPage } from '../common/page/MainPage';
+import { SearchPage } from '../common/page/SearchPage';
 
 export function Router() {
 	return (
 		<Routes>
 			<Route path='/' element={<MainLayout />}>
 				<Route index element={<MainPage />} />
+				<Route path='search' element={<SearchPage />} />
 			</Route>
 			<Route path='*' element={<div>not found</div>} />
 		</Routes>
---
+++ frontend/src/common/page/SearchPage.tsx
@@ -0,0 +1,18 @@
+import { memo, useContext, useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { StoreContainerContext } from '../../application';
+
+export const SearchPage = memo(function SearchPage() {
+	const [searchParam] = useSearchParams();
+	const { setInputData } = useContext(StoreContainerContext);
+
+	const data = searchParam.get('q');
+
+	useEffect(() => {
+		if (data) {
+			setInputData(data);
+		}
+	}, [data]);
+
+	return <div></div>;
+});
