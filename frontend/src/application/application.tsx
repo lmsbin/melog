4e73ab99b7728600cceac6d5b60888ff0ddb5763
Jo hanbin <whgksqls9999@naver.com>
2025-03-26 23:36:44 +0900

    resolve : setter 컴포넌트 제외

--- frontend/src/application/application.tsx
+++ frontend/src/application/application.tsx
@@ -1,13 +1,12 @@
 import { BrowserRouter } from 'react-router-dom';
 import { Router } from './router';
-import { Setter } from './setter';
 
 function Application() {
 	return (
 		<BrowserRouter>
-			<Setter>
-				<Router />
-			</Setter>
+			{/* <Setter> */}
+			<Router />
+			{/* </Setter> */}
 		</BrowserRouter>
 	);
 }
