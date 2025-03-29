2a280ee4de0d593d5322d261d20a6c61f4b64dd5
Jo hanbin <whgksqls9999@naver.com>
2025-03-29 13:26:30 +0900

    add : Img 컴포넌트 추가

---
+++ frontend/src/component/img/BaseImg.tsx
@@ -0,0 +1,18 @@
+import { memo, MouseEvent, useCallback } from 'react';
+import { BaseComponentProps } from '../../type';
+
+interface ImgProps extends BaseComponentProps {
+    src: string;
+    onClick?: (e: MouseEvent<HTMLImageElement>) => void;
+}
+
+export const BaseImg = memo(function BaseImg({ className, src, ...props }: ImgProps) {
+    const onClick = useCallback(
+        (e: MouseEvent<HTMLImageElement>) => {
+            props.onClick?.(e);
+        },
+        [props.onClick],
+    );
+
+    return <img className={className} src={src} onClick={onClick} />;
+});
