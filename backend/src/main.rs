650e37c7a74bb99da6eb7001028d2d15b7550115
lms0806 <godmlzkf1@naver.com>
2025-03-29 23:05:15 +0900

    refact : 사용자 관련 url 함수화

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -18,6 +18,26 @@ use axum::{Router, extract::Extension, routing::get, routing::post};
 use std::sync::Arc;
 use tower_http::cors::{Any, CorsLayer};
 
+fn user_routes() -> Router {
+    Router::new()
+        .route("/getUserInfo", get(get_user_default_info))
+        .route("/getUserStatInfo", get(get_user_stat_info))
+        .route("/getUserHyperStatInfo", get(get_user_hyper_stat_info))
+        .route("/getUserPropensity", get(get_user_propensity))
+        .route("/getUserAbility", get(get_user_ability))
+        .route("/getUserSymbolEquipment", get(get_user_symbol_equipment))
+        .route("/getUserSetEffect", get(get_user_set_effect))
+        .route("/getUserCharacterSkill", post(get_user_characeter_skill))
+        .route(
+            "/getUserCharacterLinkSkill",
+            get(get_user_characeter_link_skill),
+        )
+        .route("/getUserVMatrix", get(get_user_v_matrix))
+        .route("/getUserHexaMatrix", get(get_user_hexa_matrix))
+        .route("/getUserDojang", get(get_user_dojang))
+        .route("/getUserItemEquipment", get(get_user_item_equipment))
+}
+
 #[tokio::main]
 async fn main() {
     let args: Vec<String> = std::env::args().collect();
@@ -39,22 +59,7 @@ async fn main() {
     // TODO : VEC 형식으로 가져오는 값 자체가 null인 경우 예외처리 하기
     let app = Router::new()
         .route("/getOcid", post(get_ocid))
-        .route("/getUserInfo", get(get_user_default_info))
-        .route("/getUserStatInfo", get(get_user_stat_info))
-        .route("/getUserHyperStatInfo", get(get_user_hyper_stat_info))
-        .route("/getUserPropensity", get(get_user_propensity))
-        .route("/getUserAbility", get(get_user_ability))
-        .route("/getUserSymbolEquipment", get(get_user_symbol_equipment))
-        .route("/getUserSetEffect", get(get_user_set_effect))
-        .route("/getUserCharacterSkill", post(get_user_characeter_skill))
-        .route(
-            "/getUserCharacterLinkSkill",
-            get(get_user_characeter_link_skill),
-        )
-        .route("/getUserVMatrix", get(get_user_v_matrix))
-        .route("/getUserHexaMatrix", get(get_user_hexa_matrix))
-        .route("/getUserDojang", get(get_user_dojang))
-        .route("/getUserItemEquipment", get(get_user_item_equipment))
+        .merge(user_routes())
         .layer(Extension(api_key))
         .layer(cors);
 
