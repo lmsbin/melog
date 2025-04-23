64d693fa8774792e4f0fbb7d293390deb0410f21
Jo hanbin <whgksqls9999@naver.com>
2025-04-23 20:41:48 +0900

    add : node start

--- start.js
+++ start.js
@@ -1,6 +1,10 @@
 const { exec } = require('child_process');
 const fs = require('fs/promises');
 
+// 프로젝트 루트에 .env 파일 생성
+// .env 파일 내에 API_KEY=@@@@@@@ 설정
+// 터미널에 node start
+
 (async function main() {
 	const file = await fs.readFile('.env', 'utf-8');
 	const map = new Map(file.split('\r\n').map((x) => x.split('=')));
