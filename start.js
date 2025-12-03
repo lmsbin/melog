6be44b78bb94538cfa686bbe153e19d6b962544e
Jo hanbin <whgksqls9999@naver.com>
2025-12-04 00:52:49 +0900

    feat: enhance node start command

--- start.js
+++ start.js
@@ -5,6 +5,15 @@ const fs = require('fs/promises');
 // .env 파일 내에 API_KEY=@@@@@@@ 설정
 // 터미널에 node start
 
+// 브라우저를 여는 함수 (Windows 전용)
+function openBrowser(url) {
+	exec(`start "" "${url}"`, (error) => {
+		if (error) {
+			console.error('브라우저를 열 수 없습니다:', error);
+		}
+	});
+}
+
 (async function main() {
 	const file = await fs.readFile('.env', 'utf-8');
 	const map = new Map(file.split('\r\n').map((x) => x.split('=')));
@@ -17,4 +26,9 @@ const fs = require('fs/promises');
 
 	exec(`cd backend/src && cargo run ${API_KEY}`, { stdio: 'inherit' });
 	exec('cd frontend && npm run dev', { stdio: 'inherit' });
+
+	// 프론트엔드 서버가 시작될 시간을 기다린 후 브라우저 열기
+	setTimeout(() => {
+		openBrowser('http://localhost:5173');
+	}, 3000); // 3초 후 브라우저 열기
 })();
