6548a8f76c3ea287da0db2f6e2797a8ec4e676d2
Jo hanbin <whgksqls9999@naver.com>
2026-01-03 11:02:47 +0900

    chore: v-matrix 타입 수정

--- frontend/src/features/user-info/types/v-matrix.ts
+++ frontend/src/features/user-info/types/v-matrix.ts
@@ -4,17 +4,12 @@
 
 export interface VCoreEquipment {
 	slot_id: string;
-	slot_level: number;
 	v_core_name: string;
 	v_core_type: string;
 	v_core_level: number;
-	v_core_skill_1: string;
-	v_core_skill_2: string;
-	v_core_skill_3: string;
 }
 
 export interface UserVMatrix {
 	character_v_core_equipment: VCoreEquipment[];
 	character_v_matrix_remain_slot_upgrade_point: number;
 }
-
